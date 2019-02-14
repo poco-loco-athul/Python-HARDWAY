e7c62bf8af0a0c18d7a83c8661bcf8f8d56b8db7
athul <kpathul@outlook.com>
2019-02-14 09:09:57 +0530

    Finishes Ex28: Boolean Practice

---
+++ ex28.py
@@ -0,0 +1,10 @@
+#Ex28: Boolean Practice
+
+print(3 != 4 and ("testing" != "test" or "Python" == "Python"))
+# Guess: This should be True
+
+print("Test and Test:", "Test" and "Test", type("Test" and "Test"))
+print("True and 1: ",True and 1)
+print("1 and True: ",1 and True)
+print( 1 and False)
+
