c3bc7d4d08972563c6a24ac28bd57756823fcad9
athul <kpathul@outlook.com>
2019-02-14 09:28:36 +0530

    Finishes Ex30: Else and If

---
+++ ex30.py
@@ -0,0 +1,12 @@
+#Ex30: Else and If
+
+apples  = 30
+oranges = 30
+
+if apples > oranges:
+    print("applesss")
+elif apples < oranges:
+    print("orangesss")
+else:
+    print("ora...pples!!")
+    
